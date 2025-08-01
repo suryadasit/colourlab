be5cd3f94138d291eb8c0cb6f5fac24986d70a9b
Surya Das <dassurya05@gmail.com>
2025-08-01 15:18:54 +0530

    Delete Colourlab/tailwind.config.js

--- Colourlab/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("tailwindcss").Config} */
-export default {
-  darkMode: "class",   // ⬅ ADD THIS LINE
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: []
-};
-
