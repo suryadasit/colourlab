5c1ce1adf1db8868089b6cf2ff985db8196fc54d
Surya Das <dassurya05@gmail.com>
2025-08-01 15:24:33 +0530

    Add files via upload

---
+++ Colourlab/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import("tailwindcss").Config} */
+export default {
+  darkMode: "class",   // ⬅ ADD THIS LINE
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: { extend: {} },
+  plugins: []
+};
+
